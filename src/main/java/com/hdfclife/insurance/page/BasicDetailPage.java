5c27a409d648c4557e94f759ad840eadfa3a7bc5
Sudip Rai <sudip.rai10@gmail.com>
2018-08-05 11:40:30 +0530

    change in basic detail page

--- src/main/java/com/hdfclife/insurance/page/BasicDetailPage.java
+++ src/main/java/com/hdfclife/insurance/page/BasicDetailPage.java
@@ -57,7 +57,7 @@ public class BasicDetailPage {
 		assurerMobileNo.sendKeys("123456789");
 		assurerEmailId.clear();
 		assurerEmailId.sendKeys("prashant.chavan@synechron.com");
-		setCalendra("1981","Oct","10");
+		setCalendra("1975","Oct","10");
 		smoker.click();
 	}
 	
@@ -72,35 +72,4 @@ public class BasicDetailPage {
 		wait.untilElementIsClickable(driver.findElement(By.xpath("//div[contains(@data-full,'9-"+date+"')]/div/div[text()='"+date+"']")));
 		driver.findElement(By.xpath("//div[contains(@data-full,'9-20')]/div/div[text()='20']")).click();
 	}
-	
-//	private void year(String year, String preYear,String postYear, String yYear) {
-//		while (Integer.parseInt(yYear) > Integer.parseInt(driver.findElement(By.xpath(year)).getText())) {
-//			driver.findElement(By.xpath(postYear)).click();
-//		}
-//	}
-//	
-//	private void month(String mon, String preMon,String postMon, String mMonth) {
-//		while (!mMonth.equals(driver.findElement(By.xpath(mon)).getText())) {
-//			driver.findElement(By.xpath(postMon)).click();
-//		}
-//	}
-//	
-//	private void date(String dateXpath1, String dateXpath2, String dateXpath3,String dDate) {
-//		Boolean flag = false;
-//		for (int i = 1; i <= 6; i++) {
-//			for (int j = 1; j <= 7; j++) {
-//				String xpath = dateXpath1 + i + dateXpath2 + j + dateXpath3;
-//				if (dDate.equals(driver.findElement(By.xpath(xpath)).getText())) {
-//					wait.untilElementIsClickable(driver.findElement(By.xpath(xpath)));
-//					driver.findElement(By.xpath(xpath)).click();
-//					flag = true;
-//					break;
-//				}
-//			}
-//			if (flag == true) {
-//				break;
-//			}
-//		}
-//
-//	}
 }
